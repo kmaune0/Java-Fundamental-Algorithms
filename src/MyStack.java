b323319e3a692e6998d3fab930f072c0d6640226
kmaune0 <kmaune71@gmail.com>
2014-06-13 10:54:28 -0700

    added example for implementing stack
    this example shows how to implement stack by creating user defined
    push() method for entering elements and pop() method for retriving
    elements from the stack.

---
+++ src/MyStack.java
@@ -0,0 +1,54 @@
+// MyStack.java
+// (C) 2014 kmaune0
+// this example shows how to implement stack by creating user defined push()
+// method for entering elements and pop() method for retriving elements from
+// the stack.
+
+public class MyStack {
+  private int maxSize;
+  private long[] stackArray;
+  private int top;
+
+  public MyStack(int s) {
+    maxSize = s;
+    stackArray = new long[maxSize];
+    top = -1;
+  }
+
+  public void push(long j) {
+    stackArray[++top] = j;
+  }
+
+  public long pop() {
+    return stackArray[top--];
+  }
+
+  public long peek() {
+    return stackArray[top];
+  }
+
+  public boolean isEmpty() {
+    return (top == -1);
+  }
+
+  public boolean isFull() {
+    return (top == maxSize - 1);
+  }
+
+  public static void main(String[] args) {
+    MyStack theStack = new MyStack(10);
+    theStack.push(10);
+    theStack.push(20);
+    theStack.push(30);
+    theStack.push(40);
+    theStack.push(50);
+
+    while (!theStack.isEmpty()) {
+      long value = theStack.pop();
+      System.out.print(value);
+      System.out.print(" ");
+    }
+
+    System.out.println("");
+  }
+}
