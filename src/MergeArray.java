6ea14ad13ed2a9e4428f79c7d06c74cc33f2b6c2
kmaune0 <kmaune71@gmail.com>
2014-06-13 11:13:47 -0700

    added merge array file
    example shows how to merge two arrays into a single array

---
+++ src/MergeArray.java
@@ -0,0 +1,19 @@
+// MergeArray.java
+// (C) 2014 kmaune0
+// example shows how to merge two arrays into a single array
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeArray {
+
+  public static void main(String args[]) {
+    String a[] = {"A". "E", "I"};
+    String b[] = {"O", "U",};
+    List list = new ArrayList(Arrays.asList(a));
+    list.addAll(Arrays.asList(b));
+    Object[] c = list.toArray();
+    System.out.println(Arrays.toString(c));
+  }
+}
